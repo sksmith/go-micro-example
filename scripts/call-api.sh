c83695946121035cfe8dab924bda793d72733b7e
Sean K Smith <ssmith2347@gmail.com>
2021-12-09 21:57:33 -0500

    added a few more handy commands

--- scripts/call-api.sh
+++ scripts/call-api.sh
@@ -13,4 +13,29 @@ curl -i -H "content-type:application/json" -u admin:admin \
     http://localhost:8080/api/inventory/v1/sku123
 
 curl -i -H "content-type:application/json" -u admin:admin \
+    -XPUT -d'{"requestID":"reserveReq1","requester":"someperson","quantity":2}' \
     http://localhost:8080/api/inventory/v1/sku123/reservation
+
+curl -i -H "content-type:application/json" -u admin:admin \
+    -XPUT -d'{"requestID":"reserveReq2","requester":"someperson","quantity":2}' \
+    http://localhost:8080/api/inventory/v1/sku123/reservation
+
+curl -i -H "content-type:application/json" -u admin:admin \
+    -XPUT -d'{"requestID":"reserveReq3","requester":"someperson","quantity":2}' \
+    http://localhost:8080/api/inventory/v1/sku123/reservation
+
+curl -i -H "content-type:application/json" -u admin:admin \
+    -XPUT -d'{"requestID":"reserveReq4","requester":"someperson","quantity":2}' \
+    http://localhost:8080/api/inventory/v1/sku123/reservation
+
+curl -i -H "content-type:application/json" -u admin:admin \
+    http://localhost:8080/api/inventory/v1/sku123/reservation
+
+curl -i -H "content-type:application/json" -u admin:admin \
+    http://localhost:8080/api/inventory/v1/sku123/reservation?state=Open
+
+curl -i -H "content-type:application/json" -u admin:admin \
+    http://localhost:8080/api/inventory/v1/sku123/reservation?state=Closed
+
+curl -i -H "content-type:application/json" -u admin:admin \
+    http://localhost:8080/api/inventory/v1/sku123/reservation?state=InvalidState
